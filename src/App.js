bd1dfba6c3ceec0faa4b7be66e3d969a3929bff8
Desi Vasileva <72608848+dessiVVA@users.noreply.github.com>
2022-03-14 22:46:16 +0200

    Create card component, Install SASS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+import React, {useEffect} from 'react';
+import './App.scss';
+import SpellCard from './components/SpellCard';
 
 function App() {
-  return
+  const url = 'https://api.open5e.com/spells/';
+
+  const getSpells = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.results);
+  }
+
+  useEffect(() => {
+    getSpells();
+  });
+  return <SpellCard />
 }
 
 export default App;
---
+++ src/components/SpellCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SpellCard = () => {
+  return (
+    <div className="card">
+      <div className="header">
+        <h1 className="title">Spell Title</h1>
+        <p className="school">school</p>
+        <p className="level">2</p>
+      </div>
+    </div>
+  );
+};
+
+export default SpellCard;
