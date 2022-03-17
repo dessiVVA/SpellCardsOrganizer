8876dfb60252cb683f8eb5a7033b262e11a9db58
Desi Vasileva <72608848+dessiVVA@users.noreply.github.com>
2022-03-17 19:27:09 +0200

    Establish card sections

--- src/components/SpellCard.jsx
+++ src/components/SpellCard.jsx
@@ -3,11 +3,36 @@ import React from "react";
 const SpellCard = () => {
   return (
     <div className="card">
-      <div className="header">
-        <h1 className="title">Spell Title</h1>
-        <p className="school">school</p>
-        <p className="level">2</p>
-      </div>
+      <section className="header">
+        <div className="title">Spell Title</div>
+        <div className="school">school</div>
+        <div className="level">2</div>
+      </section>
+      <section className="stats">
+        <div className="casting">
+          <h3>casting time</h3>
+          <p>1 action</p>
+        </div>
+        <div className="range">
+          <h3>range</h3>
+          <p>60 feet</p>
+        </div>
+        <div className="components">
+          <h3>components</h3>
+          <p>v | s | m</p>
+        </div>
+        <div className="duration">
+          <h3>duration</h3>
+          <p>1 hour</p>
+        </div>
+      </section>
+      <section className="icons">
+
+      </section>
+      <section className="castor">
+        <p>sorcerer | warlock | wizard</p>
+        <hr />
+      </section>
     </div>
   );
 };
