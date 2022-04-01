02046c1ecb2612b1def49c32a57b748e22f0fe8c
Desi Vasileva <72608848+dessiVVA@users.noreply.github.com>
2022-04-01 23:27:10 +0300

    Introduce Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="container">
+      <h1>Spell preparation</h1>
+      
+      <div className="navigation">
+          <a href="#">concentration</a>
+          <a href="#">ritual</a>
+          <a href="#">bonus</a>
+          <a href="#">self</a>
+          <a href="#">healing</a>
+      </div>
+      
+      <div className="icons">
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
