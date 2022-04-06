3ac66226eb4fabf2a31c13322269bdb3f15e5b33
Desi Vasileva <72608848+dessiVVA@users.noreply.github.com>
2022-04-06 22:29:19 +0300

    Introduce Level component

---
+++ src/components/Level.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Level.scss'
+
+const Level = () => {
+  return (
+      <div className="flag">
+          <p>C</p>
+      </div>
+  );
+};
+
+export default Level;
